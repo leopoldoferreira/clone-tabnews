1aa0c1410ec12765c865765052f2005d473a16c0
Leopoldo Ferreira <leopoldoferreira@live.com>
2025-06-03 20:18:26 -0300

    página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Teste</h1>;
+  return (
+    <h1 style={{ color: "orange", fontFamily: "Arial", textAlign: "center" }}>
+      🚧👷‍♂️🔧 <br /> Em construção...
+    </h1>
+  );
 }
 
 export default Home;
