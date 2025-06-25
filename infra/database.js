c6b44a3dac33dec0e03480f932de95e6d4fd2bbb
Leopoldo Ferreira <leopoldoferreira@live.com>
2025-06-25 20:18:42 -0300

    return ssl check through variable

--- infra/database.js
+++ infra/database.js
@@ -22,7 +22,7 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
