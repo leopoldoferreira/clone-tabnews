4aba4d6fa3114dc8f657414b5d93d7d5eec89940
Leopoldo Ferreira <leopoldoferreira@live.com>
2025-07-30 19:01:19 -0300

    test: rename all descriptions to use new pattern

--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -6,25 +6,35 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const secondResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(secondResponse.status).toBe(200);
+
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const secondResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  expect(secondResponse.status).toBe(200);
-
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
 });
